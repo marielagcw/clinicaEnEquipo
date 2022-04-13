1ecd02ec893d94c825c4b5ec8d1bb4c0bea1b6e1
Mariela Goncalves Colaco <marielagcw@hotmail.com>
2022-04-13 13:47:40 -0300

    Avanzado pero pendiente DTO

--- src/main/java/com/example/demo/controller/PacienteController.java
+++ src/main/java/com/example/demo/controller/PacienteController.java
@@ -1,7 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.Paciente;
-import com.example.demo.service.PacienteService;
+import com.example.demo.service.impl.TurnoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +14,7 @@ import java.util.List;
 public class PacienteController {
     // ATRIBUTOS
     @Autowired
-    private PacienteService service;
+    private TurnoService.PacienteService service;
 
     /* ---------------------------------------------------------------------*/
     // POST GUARDAR
